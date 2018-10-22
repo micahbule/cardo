1a74099bf7b879426e0be011f31da4a0cc219913
Kenneth G Bastian <kenneth.g.bastian@descouvre.com>
2018-10-22 21:07:27 +0800

    updated index and package.json

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+/**
+ * @description
+ * Entry file for The Province Man's Web App
+ */
 const express = require('express');
+const path = require('path');
 const app = express();
-const http = require('http');
-const fs = require('fs');
 const port = 3300;
 
 app.use(express.static('public'));
+
+// NOTE: The __dirname is important for setting up the directory of the views
+app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -12,29 +18,19 @@ app.get('/', (req, res) => {
     title: 'Cardo - Ang Probinsyano S1',
     seasons: [2015, 2016, 2017, 2018],
     actors: [
-      {name: 'Coco Martin'},
-      {name: 'Philip Salvador'},
-      {name: 'Yassi Pressman'}
+      { name: 'Coco Martin' },
+      { name: 'Philip Salvador' },
+      { name: 'Yassi Pressman' }
     ]
   };
-  res.render('../server/views/index.pug', model);
+  res.render('index.pug', model);
 });
 
 app.get('/about', (req, res) => {
   res.send('What about us?');
 });
-//- npm install --only=dev nodemon
 
 app.listen(port, (err) => {
   if(err) { return console.error(err); }
   console.log(`Listening to ${port}...`);
 });
-
-// const server = http
-//   .createServer();
-
-// server.listen(port, (err) => {
-//   if(err) { return console.error(err); }
-//   console.log(`Listening to ${port}...`);
-// });
-
--- public/cardo.js
+++ public/cardo.js
@@ -1,6 +1,6 @@
+// IIFE - Immediately Invoked Function Expression
+// Read More: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
 (function() {
-  // IIFE
   var h1Title = document.getElementById('h1Title');
   h1Title.textContent = 'Ang Probinsyano';
-
 })();
